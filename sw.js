37369285418f98f959e97daf7538081ed2b2fe19
turtle-scanner <byungs07@gmail.com>
2026-08-11 07:37:27 +0900

    Inject super force PWA cache destruction script to instantly reload D-102 and camera fixes

--- sw.js
+++ sw.js
@@ -1,37 +1,19 @@
-const CACHE_NAME = 'timestamp-camera-v20260811-touch-start';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
+const CACHE_NAME = 'timestamp-camera-v20260811-super-clear-d102';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
+        cacheNames.map(cache => caches.delete(cache))
       );
     }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
