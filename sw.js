4fe600dd4f34b281141724836c411ef4c2118a1b
turtle-scanner <byungs07@gmail.com>
2026-08-11 07:54:05 +0900

    Purge localStorage user_location and user_nickname to completely erase old cached location text

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'timestamp-camera-v20260811-remove-location-v1';
+const CACHE_NAME = 'timestamp-camera-v20260811-purge-localstorage-location';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
